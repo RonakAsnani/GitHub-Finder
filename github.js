56292659945deb979a9c9adfb99c158a629484ca
Ronak <ronakasnani5@gmail.com>
2020-12-12 00:05:30 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor(){
+        this.client_id ='7bff041269065fcc1745';
+        this.client_secret ='5602c7fcf95e2e9734b52b1bd7eace0ae26d10e0';
+        this.reops_count = 5;
+        this.repos_sort = 'created: asc'
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.reops_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
